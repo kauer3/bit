5ff02ff2ca45d618b2470a89e668f4e5d12e1032
Kauê Reis <kaue0607@gmail.com>
2023-05-04 21:07:47 +1200

    Add files via upload

---
+++ 502/BasicMaths.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class BasicMaths {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        System.out.printf("%d + %d = %d\n", a, b, a + b);
+        System.out.printf("%d - %d = %d\n", a, b, a - b);
+        System.out.printf("%d x %d = %d\n", a, b, a * b);
+
+        scanner.close();
+    }
+}
