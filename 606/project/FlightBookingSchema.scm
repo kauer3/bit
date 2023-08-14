cc92ff39ea7329742d6606de45835056d42e66aa
Kauê Reis <kaue0607@gmail.com>
2023-08-15 09:32:34 +1200

    Add extra jade schema

---
+++ 606/project/FlightBookingSchema.scm
@@ -0,0 +1,167 @@
+﻿jadeVersionNumber "22.0.02";
+schemaDefinition
+FlightBookingSchema subschemaOf RootSchema completeDefinition;
+	setModifiedTimeStamp "kaue" "22.0.02" 2023:08:14:10:31:11.724;
+localeDefinitions
+	5129 "English (New Zealand)" schemaDefaultLocale;
+	setModifiedTimeStamp "kaue" "22.0.02" 2023:08:14:10:31:11.708;
+typeHeaders
+	Airport subclassOf Object highestOrdinal = 3, number = 2052;
+	FlightBookingSchema subclassOf RootSchemaApp transient, sharedTransientAllowed, transientAllowed, subclassSharedTransientAllowed, subclassTransientAllowed, number = 2048;
+	Flight subclassOf Object highestOrdinal = 6, number = 2054;
+	FlightPath subclassOf Object highestOrdinal = 3, number = 2053;
+	GFlightBookingSchema subclassOf RootSchemaGlobal transient, sharedTransientAllowed, transientAllowed, subclassSharedTransientAllowed, subclassTransientAllowed, number = 2049;
+	Passenger subclassOf Object highestOrdinal = 10, number = 2055;
+	Plane subclassOf Object highestOrdinal = 3, number = 2051;
+	Ticket subclassOf Object highestOrdinal = 7, number = 2056;
+	SFlightBookingSchema subclassOf RootSchemaSession transient, sharedTransientAllowed, transientAllowed, subclassSharedTransientAllowed, subclassTransientAllowed, number = 2050;
+membershipDefinitions
+typeDefinitions
+	Object completeDefinition
+	(
+	)
+	Airport completeDefinition
+	(
+		setModifiedTimeStamp "kaue" "22.0.02" 2023:08:14:10:49:34.088;
+	attributeDefinitions
+		cityCode:                      String[4] readonly, number = 3, ordinal = 3;
+		setModifiedTimeStamp "kaue" "22.0.02" 2023:08:14:10:53:09.597;
+		cityName:                      String[26] readonly, number = 2, ordinal = 2;
+		setModifiedTimeStamp "kaue" "22.0.02" 2023:08:14:10:52:33.107;
+		code:                          String[4] readonly, number = 1, ordinal = 1;
+		setModifiedTimeStamp "kaue" "22.0.02" 2023:08:14:10:51:27.969;
+	)
+	Application completeDefinition
+	(
+	)
+	RootSchemaApp completeDefinition
+	(
+	)
+	FlightBookingSchema completeDefinition
+	(
+		setModifiedTimeStamp "kaue" "22.0.02" 2023:08:14:10:31:11.724;
+	)
+	Flight completeDefinition
+	(
+		setModifiedTimeStamp "kaue" "22.0.02" 2023:08:14:10:56:43.290;
+	attributeDefinitions
+		date:                          Date readonly, number = 3, ordinal = 3;
+		setModifiedTimeStamp "kaue" "22.0.02" 2023:08:14:10:57:29.526;
+		flightPath:                    Integer readonly, number = 5, ordinal = 5;
+		setModifiedTimeStamp "kaue" "22.0.02" 2023:08:14:10:58:44.715;
+		flightStatus:                  String[26] readonly, number = 6, ordinal = 6;
+		setModifiedTimeStamp "kaue" "22.0.02" 2023:08:14:10:59:31.988;
+		id:                            Integer readonly, number = 1, ordinal = 1;
+		setModifiedTimeStamp "kaue" "22.0.02" 2023:08:14:10:57:00.878;
+		plane:                         String[31] readonly, number = 4, ordinal = 4;
+		setModifiedTimeStamp "kaue" "22.0.02" 2023:08:14:10:58:29.533;
+		time:                          Time readonly, number = 2, ordinal = 2;
+		setModifiedTimeStamp "kaue" "22.0.02" 2023:08:14:10:57:18.116;
+	)
+	FlightPath completeDefinition
+	(
+		setModifiedTimeStamp "kaue" "22.0.02" 2023:08:14:10:53:42.277;
+	attributeDefinitions
+		arrivalAirport:                String[4] readonly, number = 3, ordinal = 3;
+		setModifiedTimeStamp "kaue" "22.0.02" 2023:08:14:10:56:23.693;
+		departureAirport:              String[4] readonly, number = 2, ordinal = 2;
+		setModifiedTimeStamp "kaue" "22.0.02" 2023:08:14:10:56:08.087;
+		id:                            Integer readonly, number = 1, ordinal = 1;
+		setModifiedTimeStamp "kaue" "22.0.02" 2023:08:14:10:55:27.519;
+	)
+	Global completeDefinition
+	(
+	)
+	RootSchemaGlobal completeDefinition
+	(
+	)
+	GFlightBookingSchema completeDefinition
+	(
+		setModifiedTimeStamp "kaue" "22.0.02" 2023:08:14:10:31:11.724;
+	)
+	Passenger completeDefinition
+	(
+		setModifiedTimeStamp "kaue" "22.0.02" 2023:08:14:10:59:54.048;
+	attributeDefinitions
+		address:                       String[31] readonly, number = 8, ordinal = 8;
+		setModifiedTimeStamp "kaue" "22.0.02" 2023:08:14:11:03:05.829;
+		dateOfBirth:                   Date readonly, number = 4, ordinal = 4;
+		setModifiedTimeStamp "kaue" "22.0.02" 2023:08:14:11:01:05.079;
+		email:                         String[31] readonly, number = 10, ordinal = 10;
+		setModifiedTimeStamp "kaue" "22.0.02" 2023:08:14:11:05:28.237;
+		fullName:                      String[31] readonly, number = 3, ordinal = 3;
+		setModifiedTimeStamp "kaue" "22.0.02" 2023:08:14:11:00:47.313;
+		id:                            Integer readonly, number = 1, ordinal = 1;
+		setModifiedTimeStamp "kaue" "22.0.02" 2023:08:14:11:00:17.184;
+		nationality:                   String[26] readonly, number = 7, ordinal = 7;
+		setModifiedTimeStamp "kaue" "22.0.02" 2023:08:14:11:02:59.168;
+		passport:                      String[26] readonly, number = 6, ordinal = 6;
+		setModifiedTimeStamp "kaue" "22.0.02" 2023:08:14:11:02:38.968;
+		phoneNumber:                   String[26] readonly, number = 9, ordinal = 9;
+		setModifiedTimeStamp "kaue" "22.0.02" 2023:08:14:11:05:04.081;
+		staff:                         Boolean readonly, number = 5, ordinal = 5;
+		setModifiedTimeStamp "kaue" "22.0.02" 2023:08:14:11:01:40.240;
+		title:                         String[31] readonly, number = 2, ordinal = 2;
+		setModifiedTimeStamp "kaue" "22.0.02" 2023:08:14:11:00:32.611;
+	)
+	Plane completeDefinition
+	(
+		setModifiedTimeStamp "kaue" "22.0.02" 2023:08:14:10:35:02.899;
+	attributeDefinitions
+		numberOfSeats:                 Integer readonly, number = 3, ordinal = 3;
+		setModifiedTimeStamp "kaue" "22.0.02" 2023:08:14:10:46:33.231;
+		size:                          String[11] readonly, number = 2, ordinal = 2;
+		setModifiedTimeStamp "kaue" "22.0.02" 2023:08:14:10:44:34.535;
+		type:                          String[26] readonly, number = 1, ordinal = 1;
+		setModifiedTimeStamp "kaue" "22.0.02" 2023:08:14:10:37:51.332;
+	)
+	Ticket completeDefinition
+	(
+		setModifiedTimeStamp "kaue" "22.0.02" 2023:08:14:11:07:12.952;
+	attributeDefinitions
+		baggage:                       String[26] readonly, number = 5, ordinal = 5;
+		setModifiedTimeStamp "kaue" "22.0.02" 2023:08:14:11:09:28.214;
+		flightId:                      Integer readonly, number = 3, ordinal = 3;
+		setModifiedTimeStamp "kaue" "22.0.02" 2023:08:14:11:08:36.332;
+		id:                            Integer readonly, number = 1, ordinal = 1;
+		setModifiedTimeStamp "kaue" "22.0.02" 2023:08:14:11:07:26.520;
+		passengerId:                   Integer readonly, number = 2, ordinal = 2;
+		setModifiedTimeStamp "kaue" "22.0.02" 2023:08:14:11:08:08.045;
+		paymentStatus:                 String[26] readonly, number = 6, ordinal = 6;
+		setModifiedTimeStamp "kaue" "22.0.02" 2023:08:14:11:10:12.726;
+		price:                         Decimal[12] readonly, number = 7, ordinal = 7;
+		setModifiedTimeStamp "kaue" "22.0.02" 2023:08:14:11:10:35.330;
+		seatNumber:                    String[9] readonly, number = 4, ordinal = 4;
+		setModifiedTimeStamp "kaue" "22.0.02" 2023:08:14:11:08:59.721;
+	)
+	WebSession completeDefinition
+	(
+	)
+	RootSchemaSession completeDefinition
+	(
+		setModifiedTimeStamp "<unknown>" "6.1.00" 20031119 2003:12:01:13:54:02.270;
+	)
+	SFlightBookingSchema completeDefinition
+	(
+		setModifiedTimeStamp "kaue" "22.0.02" 2023:08:14:10:31:11.724;
+	)
+databaseDefinitions
+	FlightBookingSchemaDb
+	(
+	setModifiedTimeStamp "kaue" "22.0.02" 2023:08:14:10:31:11.724;
+	databaseFileDefinitions
+		"flightbook" number = 51;
+		setModifiedTimeStamp "kaue" "22.0.02" 2023:08:14:10:31:11.724;
+	defaultFileDefinition "flightbook";
+	classMapDefinitions
+		Airport in "flightbook";
+		Flight in "flightbook";
+		FlightBookingSchema in "_usergui";
+		FlightPath in "flightbook";
+		GFlightBookingSchema in "flightbook";
+		Passenger in "flightbook";
+		Plane in "flightbook";
+		SFlightBookingSchema in "_environ";
+		Ticket in "flightbook";
+	)
+typeSources
---
+++ 606/simulateATM exercise/BankingModelSchema.scm
@@ -0,0 +1,260 @@
+﻿jadeVersionNumber "22.0.02";
+schemaDefinition
+BankingModelSchema subschemaOf RootSchema completeDefinition;
+	setModifiedTimeStamp "kaue" "22.0.02" 2023:07:25:10:06:56;
+constantDefinitions
+	categoryDefinition FinancialConstants
+		setModifiedTimeStamp "kaue" "22.0.02" 2023:08:08:09:46:22.621;
+		TaxRate:                       Decimal = 0.15;
+		setModifiedTimeStamp "kaue" "22.0.02" 2023:08:08:09:48:00.173;
+localeDefinitions
+	5129 "English (New Zealand)" schemaDefaultLocale;
+	setModifiedTimeStamp "kaue" "22.0.02" 2023:07:25:10:06:55.936;
+typeHeaders
+	BankingModelSchema subclassOf RootSchemaApp transient, sharedTransientAllowed, transientAllowed, subclassSharedTransientAllowed, subclassTransientAllowed, number = 2062;
+	BankAccount subclassOf Object abstract, highestOrdinal = 3, number = 2067;
+	ChequeAccount subclassOf BankAccount highestOrdinal = 1, number = 2068;
+	SavingsAccount subclassOf BankAccount highestOrdinal = 1, number = 2069;
+	Customer subclassOf Object highestOrdinal = 4, number = 2066;
+	GBankingModelSchema subclassOf RootSchemaGlobal transient, sharedTransientAllowed, transientAllowed, subclassSharedTransientAllowed, subclassTransientAllowed, number = 2063;
+	SBankingModelSchema subclassOf RootSchemaSession transient, sharedTransientAllowed, transientAllowed, subclassSharedTransientAllowed, subclassTransientAllowed, number = 2064;
+membershipDefinitions
+typeDefinitions
+	Object completeDefinition
+	(
+	)
+	Application completeDefinition
+	(
+	)
+	RootSchemaApp completeDefinition
+	(
+	)
+	BankingModelSchema completeDefinition
+	(
+		setModifiedTimeStamp "kaue" "22.0.02" 2023:07:25:10:06:56;
+	)
+	BankAccount completeDefinition
+	(
+		setModifiedTimeStamp "kaue" "22.0.02" 2023:08:01:10:34:03.221;
+	attributeDefinitions
+		balance:                       Decimal[12,2] readonly, number = 1, ordinal = 1;
+		setModifiedTimeStamp "kaue" "22.0.02" 2023:07:25:10:25:31.765;
+		number:                        Integer readonly, number = 2, ordinal = 2;
+		setModifiedTimeStamp "kaue" "22.0.02" 2023:07:25:10:26:03.878;
+	jadeMethodDefinitions
+		withdraw(amount: Decimal) updating, number = 1001;
+		setModifiedTimeStamp "kaue" "22.0.02" 2023:08:14:14:11:00.270;
+	)
+	ChequeAccount completeDefinition
+	(
+		setModifiedTimeStamp "kaue" "22.0.02" 2023:08:01:09:34:31.190;
+	attributeDefinitions
+		overdraftLimit:                Decimal[12] readonly, number = 1, ordinal = 1;
+		setModifiedTimeStamp "kaue" "22.0.02" 2023:07:25:10:29:09.067;
+	jadeMethodDefinitions
+		setPropertiesOnCreate(
+			pBalance: Decimal; 
+			pOverdraft: Decimal) updating, number = 1001;
+		setModifiedTimeStamp "kaue" "22.0.02" 2023:08:01:10:08:05.177;
+	)
+	SavingsAccount completeDefinition
+	(
+		setModifiedTimeStamp "kaue" "22.0.02" 2023:08:01:09:54:31.587;
+	attributeDefinitions
+		interestRate:                  Decimal[3] readonly, number = 1, ordinal = 1;
+		setModifiedTimeStamp "kaue" "22.0.02" 2023:07:25:10:30:40.928;
+	jadeMethodDefinitions
+		setPropertiesOnCreate(
+			pBalance: Decimal; 
+			pIntRate: Decimal) updating, number = 1001;
+		setModifiedTimeStamp "kaue" "22.0.02" 2023:08:01:10:16:11.572;
+	)
+	Customer completeDefinition
+	(
+		setModifiedTimeStamp "kaue" "22.0.02" 2023:07:25:10:12:33.636;
+	attributeDefinitions
+		address:                       String[26] readonly, number = 1, ordinal = 1;
+		setModifiedTimeStamp "kaue" "22.0.02" 2023:07:25:10:17:52.586;
+		firstName:                     String[26] readonly, number = 2, ordinal = 2;
+		setModifiedTimeStamp "kaue" "22.0.02" 2023:07:25:10:21:43.150;
+		lastName:                      String[16] readonly, number = 3, ordinal = 3;
+		setModifiedTimeStamp "kaue" "22.0.02" 2023:07:25:10:22:49.731;
+		number:                        Integer readonly, number = 4, ordinal = 4;
+		setModifiedTimeStamp "kaue" "22.0.02" 2023:07:25:10:23:12.392;
+	)
+	Global completeDefinition
+	(
+	)
+	RootSchemaGlobal completeDefinition
+	(
+	)
+	GBankingModelSchema completeDefinition
+	(
+		setModifiedTimeStamp "kaue" "22.0.02" 2023:07:25:10:06:56;
+	)
+	JadeScript completeDefinition
+	(
+	jadeMethodDefinitions
+		createChequeAccounts() number = 1001;
+		setModifiedTimeStamp "kaue" "22.0.02" 2023:08:01:10:10:36.916;
+		createSavingsAccounts() number = 1002;
+		setModifiedTimeStamp "kaue" "22.0.02" 2023:08:14:14:16:26.390;
+		simulateATM() number = 1004;
+		setModifiedTimeStamp "kaue" "22.0.02" 2023:08:14:14:16:33.598;
+		testAddTax() number = 1003;
+		setModifiedTimeStamp "kaue" "22.0.02" 2023:08:08:09:42:21.954;
+	)
+	WebSession completeDefinition
+	(
+	)
+	RootSchemaSession completeDefinition
+	(
+		setModifiedTimeStamp "<unknown>" "6.1.00" 20031119 2003:12:01:13:54:02.270;
+	)
+	SBankingModelSchema completeDefinition
+	(
+		setModifiedTimeStamp "kaue" "22.0.02" 2023:07:25:10:06:56;
+	)
+	Decimal completeDefinition
+	(
+	jadeMethodDefinitions
+		addTax(): Decimal number = 1001;
+		setModifiedTimeStamp "kaue" "22.0.02" 2023:08:08:09:49:58.756;
+	)
+databaseDefinitions
+	BankingModelSchemaDb
+	(
+	setModifiedTimeStamp "kaue" "22.0.02" 2023:07:25:10:06:56;
+	databaseFileDefinitions
+		"bank" number = 52;
+		setModifiedTimeStamp "kaue" "22.0.02" 2023:07:25:10:06:56;
+	defaultFileDefinition "bank";
+	classMapDefinitions
+		BankAccount in "bank";
+		BankingModelSchema in "_usergui";
+		ChequeAccount in "bank";
+		Customer in "bank";
+		GBankingModelSchema in "bank";
+		SBankingModelSchema in "_environ";
+		SavingsAccount in "bank";
+	)
+typeSources
+	BankAccount (
+	jadeMethodSources
+withdraw
+{
+withdraw(amount: Decimal) updating;
+begin
+	self.balance := self.balance - amount;
+end;
+}
+	)
+	ChequeAccount (
+	jadeMethodSources
+setPropertiesOnCreate
+{
+setPropertiesOnCreate(pBalance : Decimal; pOverdraft : Decimal) updating;
+
+begin
+	self.balance := pBalance;
+	self.overdraftLimit := pOverdraft;
+end;
+}
+	)
+	SavingsAccount (
+	jadeMethodSources
+setPropertiesOnCreate
+{
+setPropertiesOnCreate(pBalance : Decimal; pIntRate : Decimal) updating;
+
+begin
+	self.balance := pBalance;
+	self.interestRate := pIntRate;
+end;
+}
+	)
+	JadeScript (
+	jadeMethodSources
+createChequeAccounts
+{
+createChequeAccounts();
+
+vars
+	chequeAcc : ChequeAccount;
+
+begin
+	beginTransaction;
+	create chequeAcc persistent;
+	chequeAcc.setPropertiesOnCreate (100, 5000);
+	create chequeAcc persistent;
+	chequeAcc.setPropertiesOnCreate (650, 2000);
+	commitTransaction;
+end;
+}
+createSavingsAccounts
+{
+createSavingsAccounts();
+
+vars
+	savingsAcc : SavingsAccount;
+	
+begin
+	beginTransaction;
+	create savingsAcc persistent;
+	savingsAcc.setPropertiesOnCreate (20000, 0.05);
+	create savingsAcc persistent;
+	savingsAcc.setPropertiesOnCreate (1550, 0.045);
+	commitTransaction;
+end;
+}
+simulateATM
+{
+simulateATM();
+
+vars
+	accountType: String;
+	ba: BankAccount;
+	amount: Decimal[12,2];
+begin
+	write 'Enter "cheque" or "savings"';
+	read accountType;
+	if accountType = "cheque" then
+		ba := ChequeAccount.firstInstance();
+		write "Balance of cheque account = " & ba.balance.String;
+	elseif accountType = "savings" then
+		ba := SavingsAccount.firstInstance();
+		write "Balance of savings account = " & ba.balance.String;
+	endif;
+	
+	write "Enter amount to withdraw";
+	read amount;
+	
+	beginTransaction;
+	ba.withdraw(amount);
+	commitTransaction;
+	write "New balance of account = " & ba.balance.String;
+end;
+}
+testAddTax
+{
+testAddTax();
+
+vars
+	d : Decimal[8,2];
+begin
+	d := 100;
+	write d.addTax();
+end;
+}
+	)
+	Decimal (
+	jadeMethodSources
+addTax
+{
+addTax(): Decimal;
+
+begin
+	return  self + self * TaxRate;
+end;
+}
+	)
