f90c119cc945353d8afc05e1425573f4ed29345f
Kauê Reis <kaue0607@gmail.com>
2024-03-05 11:34:14 +1300

    Add 607 Lab 2

---
+++ 607/labs/TestingCards/TestingCards/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Drawing;
+using PlayingCards;
+
+namespace TestingCards
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Deck deck = new Deck();
+            for (int i = 0; i < 52; i++)
+            {
+                PlayingCard? card = deck.DealTopCard();
+                System.Console.WriteLine(card);
+            }
+        }
+    }
+}
