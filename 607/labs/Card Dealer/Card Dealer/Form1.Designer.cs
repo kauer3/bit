f6149b2ffb03a0efd5ee4486b1c332aa682ac4a6
Kauê Reis <kaue0607@gmail.com>
2024-03-20 00:10:58 +1300

    Update Card Dealer

--- 607/labs/Card Dealer/Card Dealer/Form1.Designer.cs
+++ 607/labs/Card Dealer/Card Dealer/Form1.Designer.cs
@@ -32,6 +32,11 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             imageList1 = new ImageList(components);
             comboBox1 = new ComboBox();
+            dealButton = new Button();
+            saveButton = new Button();
+            loadButton = new Button();
+            statusTimer = new System.Windows.Forms.Timer(components);
+            printButton = new Button();
             SuspendLayout();
             // 
             // imageList1
@@ -39,74 +44,119 @@
             imageList1.ColorDepth = ColorDepth.Depth32Bit;
             imageList1.ImageStream = (ImageListStreamer)resources.GetObject("imageList1.ImageStream");
             imageList1.TransparentColor = Color.Transparent;
-            imageList1.Images.SetKeyName(0, "clubs-2-75.png");
-            imageList1.Images.SetKeyName(1, "clubs-3-75.png");
-            imageList1.Images.SetKeyName(2, "clubs-4-75.png");
-            imageList1.Images.SetKeyName(3, "clubs-5-75.png");
-            imageList1.Images.SetKeyName(4, "clubs-6-75.png");
-            imageList1.Images.SetKeyName(5, "clubs-7-75.png");
-            imageList1.Images.SetKeyName(6, "clubs-8-75.png");
-            imageList1.Images.SetKeyName(7, "clubs-9-75.png");
-            imageList1.Images.SetKeyName(8, "clubs-10-75.png");
-            imageList1.Images.SetKeyName(9, "clubs-a-75.png");
+            imageList1.Images.SetKeyName(0, "clubs-a-75.png");
+            imageList1.Images.SetKeyName(1, "clubs-2-75.png");
+            imageList1.Images.SetKeyName(2, "clubs-3-75.png");
+            imageList1.Images.SetKeyName(3, "clubs-4-75.png");
+            imageList1.Images.SetKeyName(4, "clubs-5-75.png");
+            imageList1.Images.SetKeyName(5, "clubs-6-75.png");
+            imageList1.Images.SetKeyName(6, "clubs-7-75.png");
+            imageList1.Images.SetKeyName(7, "clubs-8-75.png");
+            imageList1.Images.SetKeyName(8, "clubs-9-75.png");
+            imageList1.Images.SetKeyName(9, "clubs-10-75.png");
             imageList1.Images.SetKeyName(10, "clubs-j-75.png");
-            imageList1.Images.SetKeyName(11, "clubs-k-75.png");
-            imageList1.Images.SetKeyName(12, "clubs-q-75.png");
-            imageList1.Images.SetKeyName(13, "diamonds-2-75.png");
-            imageList1.Images.SetKeyName(14, "diamonds-3-75.png");
-            imageList1.Images.SetKeyName(15, "diamonds-4-75.png");
-            imageList1.Images.SetKeyName(16, "diamonds-5-75.png");
-            imageList1.Images.SetKeyName(17, "diamonds-6-75.png");
-            imageList1.Images.SetKeyName(18, "diamonds-7-75.png");
-            imageList1.Images.SetKeyName(19, "diamonds-8-75.png");
-            imageList1.Images.SetKeyName(20, "diamonds-9-75.png");
-            imageList1.Images.SetKeyName(21, "diamonds-10-75.png");
-            imageList1.Images.SetKeyName(22, "diamonds-a-75.png");
+            imageList1.Images.SetKeyName(11, "clubs-q-75.png");
+            imageList1.Images.SetKeyName(12, "clubs-k-75.png");
+            imageList1.Images.SetKeyName(13, "diamonds-a-75.png");
+            imageList1.Images.SetKeyName(14, "diamonds-2-75.png");
+            imageList1.Images.SetKeyName(15, "diamonds-3-75.png");
+            imageList1.Images.SetKeyName(16, "diamonds-4-75.png");
+            imageList1.Images.SetKeyName(17, "diamonds-5-75.png");
+            imageList1.Images.SetKeyName(18, "diamonds-6-75.png");
+            imageList1.Images.SetKeyName(19, "diamonds-7-75.png");
+            imageList1.Images.SetKeyName(20, "diamonds-8-75.png");
+            imageList1.Images.SetKeyName(21, "diamonds-9-75.png");
+            imageList1.Images.SetKeyName(22, "diamonds-10-75.png");
             imageList1.Images.SetKeyName(23, "diamonds-j-75.png");
-            imageList1.Images.SetKeyName(24, "diamonds-k-75.png");
-            imageList1.Images.SetKeyName(25, "diamonds-q-75.png");
-            imageList1.Images.SetKeyName(26, "hearts-2-75.png");
-            imageList1.Images.SetKeyName(27, "hearts-3-75.png");
-            imageList1.Images.SetKeyName(28, "hearts-4-75.png");
-            imageList1.Images.SetKeyName(29, "hearts-5-75.png");
-            imageList1.Images.SetKeyName(30, "hearts-6-75.png");
-            imageList1.Images.SetKeyName(31, "hearts-7-75.png");
-            imageList1.Images.SetKeyName(32, "hearts-8-75.png");
-            imageList1.Images.SetKeyName(33, "hearts-9-75.png");
-            imageList1.Images.SetKeyName(34, "hearts-10-75.png");
-            imageList1.Images.SetKeyName(35, "hearts-a-75.png");
+            imageList1.Images.SetKeyName(24, "diamonds-q-75.png");
+            imageList1.Images.SetKeyName(25, "diamonds-k-75.png");
+            imageList1.Images.SetKeyName(26, "hearts-a-75.png");
+            imageList1.Images.SetKeyName(27, "hearts-2-75.png");
+            imageList1.Images.SetKeyName(28, "hearts-3-75.png");
+            imageList1.Images.SetKeyName(29, "hearts-4-75.png");
+            imageList1.Images.SetKeyName(30, "hearts-5-75.png");
+            imageList1.Images.SetKeyName(31, "hearts-6-75.png");
+            imageList1.Images.SetKeyName(32, "hearts-7-75.png");
+            imageList1.Images.SetKeyName(33, "hearts-8-75.png");
+            imageList1.Images.SetKeyName(34, "hearts-9-75.png");
+            imageList1.Images.SetKeyName(35, "hearts-10-75.png");
             imageList1.Images.SetKeyName(36, "hearts-j-75.png");
-            imageList1.Images.SetKeyName(37, "hearts-k-75.png");
-            imageList1.Images.SetKeyName(38, "hearts-q-75.png");
-            imageList1.Images.SetKeyName(39, "spades-2-75.png");
-            imageList1.Images.SetKeyName(40, "spades-3-75.png");
-            imageList1.Images.SetKeyName(41, "spades-4-75.png");
-            imageList1.Images.SetKeyName(42, "spades-5-75.png");
-            imageList1.Images.SetKeyName(43, "spades-6-75.png");
-            imageList1.Images.SetKeyName(44, "spades-7-75.png");
-            imageList1.Images.SetKeyName(45, "spades-8-75.png");
-            imageList1.Images.SetKeyName(46, "spades-9-75.png");
-            imageList1.Images.SetKeyName(47, "spades-10-75.png");
-            imageList1.Images.SetKeyName(48, "spades-a-75.png");
+            imageList1.Images.SetKeyName(37, "hearts-q-75.png");
+            imageList1.Images.SetKeyName(38, "hearts-k-75.png");
+            imageList1.Images.SetKeyName(39, "spades-a-75.png");
+            imageList1.Images.SetKeyName(40, "spades-2-75.png");
+            imageList1.Images.SetKeyName(41, "spades-3-75.png");
+            imageList1.Images.SetKeyName(42, "spades-4-75.png");
+            imageList1.Images.SetKeyName(43, "spades-5-75.png");
+            imageList1.Images.SetKeyName(44, "spades-6-75.png");
+            imageList1.Images.SetKeyName(45, "spades-7-75.png");
+            imageList1.Images.SetKeyName(46, "spades-8-75.png");
+            imageList1.Images.SetKeyName(47, "spades-9-75.png");
+            imageList1.Images.SetKeyName(48, "spades-10-75.png");
             imageList1.Images.SetKeyName(49, "spades-j-75.png");
-            imageList1.Images.SetKeyName(50, "spades-k-75.png");
-            imageList1.Images.SetKeyName(51, "spades-q-75.png");
+            imageList1.Images.SetKeyName(50, "spades-q-75.png");
+            imageList1.Images.SetKeyName(51, "spades-k-75.png");
             imageList1.Images.SetKeyName(52, "back-blue-75-1.png");
             // 
             // comboBox1
             // 
             comboBox1.FormattingEnabled = true;
             comboBox1.Items.AddRange(new object[] { "Hand 1", "Hand 2", "Hand 3", "Hand 4" });
-            comboBox1.Location = new Point(0, 0);
+            comboBox1.Location = new Point(10, 10);
             comboBox1.Name = "comboBox1";
             comboBox1.Size = new Size(121, 23);
             comboBox1.TabIndex = 0;
+            comboBox1.SelectedIndexChanged += SelectedIndexChanged;
+            // 
+            // dealButton
+            // 
+            dealButton.Location = new Point(146, 10);
+            dealButton.Name = "dealButton";
+            dealButton.Size = new Size(121, 23);
+            dealButton.TabIndex = 1;
+            dealButton.Text = "Deal";
+            dealButton.UseVisualStyleBackColor = true;
+            dealButton.Click += DealButton_Click;
+            // 
+            // saveButton
+            // 
+            saveButton.Location = new Point(285, 10);
+            saveButton.Name = "saveButton";
+            saveButton.Size = new Size(121, 23);
+            saveButton.TabIndex = 2;
+            saveButton.Text = "Save";
+            saveButton.UseVisualStyleBackColor = true;
+            saveButton.Click += SaveButton_Click;
+            // 
+            // loadButton
+            // 
+            loadButton.Location = new Point(423, 10);
+            loadButton.Name = "loadButton";
+            loadButton.Size = new Size(121, 23);
+            loadButton.TabIndex = 3;
+            loadButton.Text = "Load";
+            loadButton.UseVisualStyleBackColor = true;
+            loadButton.Click += LoadButton_Click;
+            // 
+            // printButton
+            // 
+            printButton.Location = new Point(561, 10);
+            printButton.Name = "printButton";
+            printButton.Size = new Size(121, 23);
+            printButton.TabIndex = 4;
+            printButton.Text = "Print";
+            printButton.UseVisualStyleBackColor = true;
+            printButton.Click += PrintButton_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(999, 450);
+            Controls.Add(printButton);
+            Controls.Add(loadButton);
+            Controls.Add(saveButton);
+            Controls.Add(dealButton);
             Controls.Add(comboBox1);
             Name = "Form1";
             Text = "Form1";
@@ -118,5 +168,10 @@
 
         private ImageList imageList1;
         private ComboBox comboBox1;
+        private Button dealButton;
+        private Button saveButton;
+        private Button loadButton;
+        private System.Windows.Forms.Timer statusTimer;
+        private Button printButton;
     }
 }
--- 607/labs/Card Dealer/Card Dealer/Form1.cs
+++ 607/labs/Card Dealer/Card Dealer/Form1.cs
@@ -1,98 +1,269 @@
+using iText.IO.Font.Constants;
+using iText.IO.Image;
+using iText.Kernel.Font;
+using iText.Kernel.Geom;
+using iText.Kernel.Pdf;
+using iText.Layout;
+using iText.Layout.Element;
+using iText.Layout.Properties;
+using Newtonsoft.Json;
 using PlayingCards;
+using System.Reflection;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Windows.Forms;
 
 namespace Card_Dealer
 {
     public partial class Form1 : Form
     {
         private Deck deck;
-        List<Image> images = new List<Image>();
+        List<System.Drawing.Image> images = new List<System.Drawing.Image>();
 
         const int HAND_SIZE = 13;
         const int CARD_WIDTH = 75;
         const int CARD_HEIGHT = 108;
 
-        Hand firstHand;
-        // Image[] secondHand;
-        // Image[] thirdHand;
-        // Image[] fourthHand;
         PictureBox[] cardImages;
+        ListOfHands hands;
+        int selectedHand = 0;
 
-        Image[][] hands;
+        StatusStrip statusStrip;
 
         public Form1()
         {
             InitializeComponent();
 
             deck = new Deck();
-
-
-            // PictureBox pbDeck = PictureBoxDeck;
-            // PictureBox pbCard = PictureBoxCard;
-
+            cardImages = new PictureBox[52];
+            statusStrip = new StatusStrip();
+            this.Controls.Add(statusStrip);
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            HandleImages();
+            DealCards();
+
+            comboBox1.SelectedIndex = 0;
+        }
+
+        private void DealButton_Click(Object sender, EventArgs e)
+        {
+            deck = new Deck();
+            images.Clear();
+            HandleImages();
+            DealCards();
+            GetSelectedHand(selectedHand);
+        }
+
+        private void SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ComboBox cb = (ComboBox)sender;
+            selectedHand = cb.SelectedIndex;
+            GetSelectedHand(selectedHand);
+        }
+
+        private void GetSelectedHand(int hand)
+        {
+            switch (hand)
+            {
+                case 0:
+                    DisplayHand(hands.firstHand);
+                    break;
+                case 1:
+                    DisplayHand(hands.secondHand);
+                    break;
+                case 2:
+                    DisplayHand(hands.thirdHand);
+                    break;
+                case 3:
+                    DisplayHand(hands.fourthHand);
+                    break;
+            }
+        }
+
+        private void DisplayHand(List<PlayingCard> hand)
+        {
+            for (int i = 0; i < hand.Count; i++)
+            {
+                cardImages[i].Image = hand[i].GetImage();
+
+                int row = i / HAND_SIZE;
+                int col = i % HAND_SIZE;
+                cardImages[i].Location = new System.Drawing.Point(10 + col * CARD_WIDTH, 45 + row * CARD_HEIGHT);
+                cardImages[i].Size = (new Size(CARD_WIDTH, CARD_HEIGHT));
+                cardImages[i].SizeMode = PictureBoxSizeMode.StretchImage;
+
+                Controls.Add(cardImages[i]);
+            }
+        }
+
+        private void HandleImages()
+        {
+            for (int i = 0; i < 52 - 1; i++)
+            {
+                if (cardImages[i] != null)
+                {
+                    cardImages[i].Dispose();
+                }
+                cardImages[i] = new PictureBox();
+            }
+
             for (int i = 0; i < imageList1.Images.Count - 1; i++)
             {
                 images.Add(imageList1.Images[i]);
             }
 
             deck.AssigImages(images, imageList1.Images[52]);
-            deck.Shuffle();
-
-            firstHand = deck.DealHand(HAND_SIZE);
-            secondHand = new Image[HAND_SIZE];
-            thirdHand = new Image[HAND_SIZE];
-            fourthHand = new Image[HAND_SIZE];
-
-            hands = new Image[][] { firstHand, secondHand, thirdHand, fourthHand };
-            DisplayHand(firstHand);
-
-            // for (int i = 0; i < HAND_SIZE; i++)
-            {
-                cardImages[i] = new PictureBox();
-                PlayingCard card = deck.DealTopCard();
-                //card.IsFaceUp = false;
-                cardImages[i].Image = card.GetImage();
-
-                int row = i / HAND_SIZE;
-                int col = i % HAND_SIZE;
-                cardImages[i].Location = new Point(10 + col * CARD_WIDTH, 10 + row * CARD_HEIGHT);
-                cardImages[i].Size = (new Size(CARD_WIDTH, CARD_HEIGHT));
-                cardImages[i].SizeMode = PictureBoxSizeMode.StretchImage;
-
-                //hands[hand][i].Click += Card_Click;
-                //cardImages[i].Tag = card;
-                //Controls.Add(cardImages[i]);
-            }
         }
 
-        private void DisplayHand(Image[] hand)
+        private void DealCards()
         {
+            deck.Shuffle();
+            hands = new ListOfHands(DealHand(), DealHand(), DealHand(), DealHand());
+        }
+
+        private List<PlayingCard> DealHand()
+        {
+            PlayingCard? card;
+            List<PlayingCard> hand = new List<PlayingCard>();
+            
             for (int i = 0; i < HAND_SIZE; i++)
             {
-                cardImages[i] = new PictureBox();
-                PlayingCard card = deck.DealTopCard();
-                card.IsFaceUp = false;
-                cardImages[i].Image = card.GetImage();
+                card = deck.DealTopCard();
+                hand.Add(card);
+            }
 
-                int row = i / HAND_SIZE;
-                int col = i % HAND_SIZE;
-                cardImages[i].Location = new Point(10 + col * CARD_WIDTH, 10 + row * CARD_HEIGHT);
-                cardImages[i].Size = (new Size(CARD_WIDTH, CARD_HEIGHT));
-                cardImages[i].SizeMode = PictureBoxSizeMode.StretchImage;
+            return hand;
+        }
 
-                cardImages[i].Tag = card;
-                Controls.Add(cardImages[i]);
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.ShowDialog();
+
+            if (saveFileDialog.FileName != "")
+            {
+#pragma warning disable SYSLIB0011 // Type or member is obsolete
+                BinaryFormatter formatter = new BinaryFormatter();
+#pragma warning restore SYSLIB0011 // Type or member is obsolete
+
+                using (FileStream stream = new FileStream(saveFileDialog.FileName, FileMode.Create))
+                {
+                    formatter.Serialize(stream, hands);
+                }
             }
         }
 
-        private void SelectedIndexChanged(object sender, EventArgs e)
+        private void LoadButton_Click(object sender, EventArgs e)
         {
-            ComboBox cb = (ComboBox)sender;
-            int hand = int.Parse(cb.Tag.ToString());
-            DisplayHand(hands[hand]);
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.ShowDialog();
+
+            if (openFileDialog.FileName != "")
+            {
+#pragma warning disable SYSLIB0011 // Type or member is obsolete
+                BinaryFormatter formatter = new BinaryFormatter();
+#pragma warning restore SYSLIB0011 // Type or member is obsolete
+                using (FileStream stream = new FileStream(openFileDialog.FileName, FileMode.Open))
+                {
+                    hands = (ListOfHands)formatter.Deserialize(stream);
+
+                    if (selectedHand == 0)
+                    {
+                        DisplayHand(hands.firstHand);
+                    }
+                    else
+                    {
+                        comboBox1.SelectedIndex = 0;
+                    }
+                }
+            }
+        }
+
+        private void PrintButton_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.ShowDialog();
+
+            if (saveFileDialog.FileName != "")
+            {
+                WriterProperties properties = new WriterProperties();
+                properties.AddXmpMetadata();
+                properties.SetPdfVersion(PdfVersion.PDF_2_0);
+
+                PdfWriter writer = new PdfWriter(saveFileDialog.FileName, properties);
+                PdfDocument pdf = new PdfDocument(writer);
+
+                PdfDocumentInfo info = pdf.GetDocumentInfo();
+                info.SetAuthor("Kauê");
+                info.SetSubject("Card Hands");
+                info.SetTitle("Card Dealer");
+
+                Document document = new Document(pdf, PageSize.A4);
+
+                statusStrip.Items.Add(new ToolStripStatusLabel("Document Created Successfully"));
+
+                statusTimer.Enabled = true;
+                statusTimer.Interval = 3000;
+                statusTimer.Start();
+
+                PdfFont mainFont = PdfFontFactory.CreateFont(StandardFonts.TIMES_ROMAN);
+                PdfFont boldFont = PdfFontFactory.CreateFont(StandardFonts.HELVETICA_BOLD);
+
+                Paragraph title = new Paragraph("PDF creation test document").SetFont(boldFont).SetFontSize(24).SetTextAlignment(TextAlignment.CENTER);
+                document.Add(title);
+
+
+                var propsList = typeof(ListOfHands).GetProperties();
+
+                foreach (var propertyName in propsList)
+                {
+                    List<PlayingCard> hand = (List<PlayingCard>)propertyName.GetValue(hands);
+
+                    foreach (PlayingCard card in hand)
+                    {
+                        System.Drawing.Image image = card.GetImage();
+                        ImageData imageData = ImageDataFactory.Create(ConvertToBytes(image));
+                        iText.Layout.Element.Image imageToSave = new iText.Layout.Element.Image(imageData);
+
+                        document.Add(imageToSave);
+                    }
+                }
+                document.Close();
+            }
+        }
+
+        private void StatusTimer_Tick(object sender, EventArgs e)
+        {
+            statusStrip.Items.Clear();
+            statusTimer.Stop();
+        }
+
+        private byte[] ConvertToBytes(System.Drawing.Image image)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                image.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+                return ms.ToArray();
+            }
+        }
+
+        [Serializable]
+        class ListOfHands
+        {
+            public List<PlayingCard> firstHand { get; set; }
+            public List<PlayingCard> secondHand { get; set; }
+            public List<PlayingCard> thirdHand { get; set; }
+            public List<PlayingCard> fourthHand { get; set; }
+
+            public ListOfHands(List<PlayingCard> firstHand, List<PlayingCard> secondHand, List<PlayingCard> thirdHand, List<PlayingCard> fourthHand)
+            {
+                this.firstHand = firstHand;
+                this.secondHand = secondHand;
+                this.thirdHand = thirdHand;
+                this.fourthHand = fourthHand;
+            }
         }
     }
 }
